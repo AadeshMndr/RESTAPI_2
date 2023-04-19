14507fb1bd1cbec778c2409281544c339fb17868
AadeshMndr <125436995+AadeshMndr@users.noreply.github.com>
2023-04-19 18:22:47 +0545

    2

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
+
+const productsRouter = require("./api/routes/products");
+
+app.use("/products", productsRouter);
+
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
